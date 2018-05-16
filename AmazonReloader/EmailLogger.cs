d54f1847d78578f7f00eb08425eb37b1c113aa0e
Beau Gosse <nofmxc@gmail.com>
2018-05-15 19:26:28 -0500

    Oops

--- AmazonReloader/EmailLogger.cs
+++ AmazonReloader/EmailLogger.cs
@@ -22,28 +22,28 @@ namespace AmazonReloader
     {
         public static void SendLog(string log)
         {
-            //var fromAddress = new MailAddress("nofmxcz@gmail.com", "Amazon Reloader");
-            //var toAddress = new MailAddress("nofmxc@gmail.com", "Beau Gosse");
-            //const string fromPassword = "zbxgozmvndffdehl";
-            //const string subject = "Amazon Reloader Failure";
+            var fromAddress = new MailAddress("nofmxcz@gmail.com", "Amazon Reloader");
+            var toAddress = new MailAddress("nofmxc@gmail.com", "Beau Gosse");
+            const string fromPassword = "zbxgozmvndffdehl";
+            const string subject = "Amazon Reloader Failure";
 
-            //var smtp = new SmtpClient
-            //{
-            //    Host = "smtp.gmail.com",
-            //    Port = 587,
-            //    EnableSsl = true,
-            //    DeliveryMethod = SmtpDeliveryMethod.Network,
-            //    UseDefaultCredentials = false,
-            //    Credentials = new NetworkCredential(fromAddress.Address, fromPassword)
-            //};
-            //using (var message = new MailMessage(fromAddress, toAddress)
-            //{
-            //    Subject = subject,
-            //    Body = log
-            //})
-            //{
-            //    smtp.Send(message);
-            //}
+            var smtp = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(fromAddress.Address, fromPassword)
+            };
+            using (var message = new MailMessage(fromAddress, toAddress)
+            {
+                Subject = subject,
+                Body = log
+            })
+            {
+                smtp.Send(message);
+            }
         }
     }
 }
